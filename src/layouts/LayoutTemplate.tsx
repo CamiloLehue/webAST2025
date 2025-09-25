9e2c3983c39c96b4d9cda2a1779f02ffe7d63e26
tryknowledge - CLehue <clehue@ast.cl>
2025-09-25 16:12:53 -0300

    feat: implement scroll to top functionality with instant and delayed options

--- src/layouts/LayoutTemplate.tsx
+++ src/layouts/LayoutTemplate.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import { useScrollToTopInstant } from "../hooks/useScrollToTop";
 
 function LayoutTemplate() {
+  useScrollToTopInstant();
+
   return (
     <div className="relative w-full min-h-screen h-full flex flex-col justify-between items-center overflow-x-hidden">
       <Header />
