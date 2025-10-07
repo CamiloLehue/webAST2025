3787a9114b1c9d876c70c4adf10c217a2ec800d4
tryknowledge - CLehue <clehue@ast.cl>
2025-10-07 14:39:03 -0300

    feat: Enhance mobile view with gradient overlay and welcome message in slider

--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -24,8 +24,18 @@ function Home() {
         `}
       >
         {isSmallDevice ? (
-          <div className="relative object-cover">
+          <div className="relative object-cover border-b-8 border-b-primary-100">
             <img src="slider/slider04.JPG" alt="Bienvenido a AST" />
+            <div className="absolute bottom-0 left-0 h-1/2 w-full bg-gradient-to-t from-bg-400"></div>
+            <div className="absolute z-10 top-[70%] -translate-y-1/2 left-[50%] transform -translate-x-1/2 p-5 flex flex-col justify-center items-center ">
+              <h2 className="text-5xl font-bold text-white ">
+                Bienvenido a AST
+              </h2>
+              <p className="text-white text-center text-balance">
+                Conectamos tecnología e innovación para transformar sectores
+                productivos clave en Chile y Latinoamérica.
+              </p>
+            </div>
           </div>
         ) : (
           <>
@@ -50,16 +60,23 @@ function Home() {
       )}
       <section id="aboutUs" className="relative w-full space-y-9 py-10">
         <FadeInSection
-          className={`max-w-7xl mx-auto grid grid-cols-2 gap-15  border border-dashed border-bg-300/10 rounded-2xl
+          className={` border border-dashed border-bg-300/10 rounded-2xl
+          ${
+            isSmallDevice
+              ? "flex justify-center items-center  max-w-sm mx-auto"
+              : "max-w-7xl mx-auto grid  gap-15 grid-cols-2"
+          }
            `}
         >
-          <SlideInLeft className="min-h-100">
-            <img
-              src="img/inicio/ia-wisensor.jpg"
-              alt=""
-              className="rounded-2xl hover:shadow-lg transition-shadow duration-300"
-            />
-          </SlideInLeft>
+          {!isSmallDevice && (
+            <SlideInLeft className="min-h-100">
+              <img
+                src="img/inicio/ia-wisensor.jpg"
+                alt=""
+                className="rounded-2xl hover:shadow-lg transition-shadow duration-300"
+              />
+            </SlideInLeft>
+          )}
           <SlideInRight className="flex flex-col justify-start pt-10 gap-4">
             <h3 className="font-bold text-primary-100 text-4xl">
               Implementamos IA
@@ -78,7 +95,11 @@ function Home() {
             </ScaleUpSection>
           </SlideInRight>
         </FadeInSection>
-        <FadeInSection className="max-w-7xl mx-auto  flex flex-col justify-center items-center">
+        <FadeInSection
+          className={` mx-auto  flex flex-col justify-center items-center
+            ${isSmallDevice ? "max-w-sm" : "max-w-7xl"}
+          `}
+        >
           <div className=" border-t-2 border-bg-300/10 px-10 py-5 rounded-t-2xl ">
             <h3 className="text-2xl font-bold text-primary-100">
               Puedes ver más sobre nosotros aquí
@@ -95,13 +116,15 @@ function Home() {
           </ScaleUpSection>
         </FadeInSection>
       </section>
-      <section id="lastNews" className="relative w-full py-15 bg-primary-100">
+      <section id="lastNews" className={`relative w-full py-15 bg-primary-100
+        ${isSmallDevice ? "max-w-lg mx-auto px-15" : "max-w-7xl mx-auto"}
+        `}>
         <Lastnews />
       </section>
       <section id="contact">
         <StaggerContainer
-          className={` gap-10 max-w-7xl mx-auto py-10 ${
-            isSmallDevice ? "flex flex-col gap-4" : "grid grid-cols-2"
+          className={` gap-10  mx-auto py-10 ${
+            isSmallDevice ? "flex flex-col gap-4 max-w-sm" : "grid grid-cols-2 max-w-7xl"
           }`}
         >
           <StaggerItem className="bg-white h-80 border border-dashed border-bg-300/10 rounded-2xl mb-4 flex flex-col justify-center items-center gap-5">
