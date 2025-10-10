25e8d642bdb6530c134634698ecc197848b92bcb
tryknowledge - CLehue <clehue@ast.cl>
2025-10-10 11:12:24 -0300

    feat: Refactor LayoutTemplate and Header components for improved loading handling and structure

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -4,28 +4,30 @@ import TopHeader from "./TopHeader";
 
 function Header() {
   return (
-    <motion.header 
-      className="relative w-full z-20 flex flex-col bg-bg-100  text-white-100"
-      initial={{ y: -100, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ 
-        duration: 0.6, 
-        ease: [0.25, 0.46, 0.45, 0.94],
-        delay: 0.1
-      }}
-    >
-      <div className="bg-bg-200 w-full">
-        <TopHeader />
-      </div>
-      <div className="max-w-7xl min-h-24 mx-auto flex justify-between items-center px-5 h-full w-full">
-        <div className="">
-          <img src="/AST-Logo-white.png" alt="Logo AST" className="w-30" />
+    <header className="sticky top-0 left-0 right-0 w-full z-[100] flex flex-col bg-bg-100 text-white-100 shadow-lg">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          duration: 0.6,
+          ease: [0.25, 0.46, 0.45, 0.94],
+          delay: 0.1,
+        }}
+        className="w-full"
+      >
+        <div className="bg-bg-200 w-full">
+          <TopHeader />
         </div>
-        <div className="relative">
-          <NavBar />
+        <div className="max-w-7xl min-h-24 mx-auto flex justify-between items-center px-5 h-full w-full">
+          <div className="">
+            <img src="/AST-Logo-white.png" alt="Logo AST" className="w-30" />
+          </div>
+          <div className="relative">
+            <NavBar />
+          </div>
         </div>
-      </div>
-    </motion.header>
+      </motion.div>
+    </header>
   );
 }
 
